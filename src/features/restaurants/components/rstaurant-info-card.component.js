fa878f633716e8bec8da2284c3c4868c22f10a16
yebin <djaak9604@gmail.com>
2023-03-12 20:01:25 +0900

    feat: mockData, tapbar

--- src/features/restaurants/components/rstaurant-info-card.component.js
+++ src/features/restaurants/components/rstaurant-info-card.component.js
@@ -1,105 +1,62 @@
 import React from "react";
-import { Text } from "react-native";
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
-import { Image, SvgXml } from "react-native-svg";
+import { SvgXml } from "react-native-svg";
 
-import { Spacer } from "../../../components/spacer/spacer.component";
-
+import { Spacer } from "../../../components//spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./rastaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
-      "https://image.ajunews.com/content/image/2018/07/25/20180725142544658121.jpg",
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
-  //* 별의 갯수만큼 새로운 인덱스를 만들어냄.
-  //* 소숫점이 들어가면 안되니까 정수로 바꿔주기
+
   const ratingArray = Array.from(new Array(Math.floor(rating)));
-  console.log(ratingArray);
 
   return (
-    <>
-      <RestaurantCard elevation={5}>
-        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-        <Info>
-          <Title>{name}</Title>
-          <Section>
-            <Rating>
-              {ratingArray.map(() => (
-                <SvgXml xml={star} width={20} height={20} />
-              ))}
-            </Rating>
-            <SectionEnd>
-              {isClosedTemporarily && (
-                <Text variant="label" style={{ color: "red" }}>
-                  CLOSED TEMPORARILY
-                </Text>
-              )}
-              <Spacer position="left" size="large">
-                {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-              </Spacer>
-              <Spacer position="left" size="large">
-                <Image
-                  style={{ width: 15, height: 15 }}
-                  source={{ uri: icon }}
-                />
-              </Spacer>
-            </SectionEnd>
-          </Section>
-          <Adress>{address}</Adress>
-        </Info>
-      </RestaurantCard>
-    </>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
   );
 };
-
-//* 스타일 컴포넌트
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Title = styled(Text)`
-  color: ${(props) => props.theme.colors.ui.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const Adress = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,31 +1,50 @@
 import React from "react";
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
-import { RestaurantInfoCard } from "../components/rstaurant-info-card.component";
 
-export const RestaurantsScreen = () => {
-  return (
-    <SafeArea>
-      <SearchbarContainer>
-        <Searchbar />
-      </SearchbarContainer>
-      <RestauranrListContainer>
-        <RestaurantInfoCard />
-      </RestauranrListContainer>
-    </SafeArea>
-  );
-};
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantInfoCard } from "../components//rstaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
-`;
-const SearchbarContainer = styled.View`
+const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
-const RestauranrListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
+  </SafeArea>
+);
